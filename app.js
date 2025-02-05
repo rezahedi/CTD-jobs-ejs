4d8d032fc472131d5162b35ceceba72311e32a0e
Reza Zahedi <r.zahedi@gmail.com>
2025-02-04 22:14:56 -0800

    Use session to store secretWord!

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ const express = require("express");
 require("express-async-errors");
 const bodyParser = require("body-parser").urlencoded({ extended: true });
 
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.use( bodyParser );
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 // secret word handling
-let secretWord = "syzygy";
 app.get("/secretWord", (req, res) => {
-  res.render("secretWord", { secretWord });
+  if ( !req.session.secretWord ) {
+    req.session.secretWord = "mY.sEcReT-wOrD";
+  }
+
+  res.render("secretWord", { secretWord: req.session.secretWord });
 });
+
 app.post("/secretWord", (req, res) => {
-  secretWord = req.body.secretWord;
+  req.session.secretWord = req.body.secretWord;
   res.redirect("/secretWord");
 });
 
